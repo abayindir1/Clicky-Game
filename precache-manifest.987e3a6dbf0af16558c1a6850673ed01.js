0b0b4aa7d039f8cbc34b36c623988acf333d83c1
Abdullah Bayindir <byndrabdullah@gmail.com>
2019-10-26 13:16:11 -0500

    Updates

---
+++ precache-manifest.987e3a6dbf0af16558c1a6850673ed01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a697eac7b21247e7209b14b86a1ab3",
+    "url": "/Clicky-Game/./index.html"
+  },
+  {
+    "revision": "8a601a0b95afe9545fa9",
+    "url": "/Clicky-Game/./static/css/main.6fd724c4.chunk.css"
+  },
+  {
+    "revision": "f7ebc6672aa134c20e5f",
+    "url": "/Clicky-Game/./static/js/2.35849069.chunk.js"
+  },
+  {
+    "revision": "8a601a0b95afe9545fa9",
+    "url": "/Clicky-Game/./static/js/main.b87092b3.chunk.js"
+  },
+  {
+    "revision": "04f1e03ce36f0a2da266",
+    "url": "/Clicky-Game/./static/js/runtime-main.d48544a8.js"
+  },
+  {
+    "revision": "549286b52183315631e20a3fcef2cfad",
+    "url": "/Clicky-Game/./static/media/ring.549286b5.png"
+  }
+]);
